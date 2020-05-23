95644f713eefe6eaa484350ccc939dba4f028d3b
Alexander Terp <alexander.terp@gmail.com>
2020-05-23 18:03:25 +1000

    1.2.8: Make compatible with beta-branch (1.4.1)
    Also include the config file, which was missing from the repo so
    far.

--- src/ArmyArrowCounter/Main.cs
+++ src/ArmyArrowCounter/Main.cs
@@ -13,7 +13,7 @@ namespace ArmyArrowCounter
             if (!IsLoaded)
             {
                 Initialize();
-                Utils.Log("Mod loaded: Army Arrow Counter v1.2.7");
+                Utils.Log("Mod loaded: Army Arrow Counter v1.2.8");
                 IsLoaded = true;
             }
         }
